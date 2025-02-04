ceae19c03dca4a86862b9e999e21969ffdb0d222
Carlos <carlosicaraballo1999@gmail.com>
2025-02-04 12:06:35 +0000

    92. Registrando interceptor en la configuración de spring

---
+++ src/main/java/com/carlos/curso/springboot/app/interceptor/springbootinterceptor/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.carlos.curso.springboot.app.interceptor.springbootinterceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+  @Autowired
+  @Qualifier("loadingTimeInterceptor")
+  private HandlerInterceptor loadingTimeInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(loadingTimeInterceptor);
+  }
+}
